1aa0e5f2c8552c8ad7723883ff06644288c781a6
Taraskyn <taraslutsyuk12@gmail.com>
2019-03-08 20:45:00 +0200

    add archivation

--- git_script.sh
+++ git_script.sh
@@ -1,1 +1,1 @@
-echo "Hello World! DevOps Git vs Jenkins task." | tee hello_world.txt
+echo "Hello World! DevOps Git vs Jenkins task." | tee hello_world.txt &&  tar -cvzf hello_world.tar.gz hello_world.txt
